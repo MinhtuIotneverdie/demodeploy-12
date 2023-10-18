4482af7bca479b19d3024587e8fe9a8db80e661b
MinhtuIotneverdie <phanminhtu19062002@gmail.com>
2023-10-18 22:55:12 +0700

    second

--- mysite/views.py
+++ mysite/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpRequest
+from django.http import HttpResponse
 
 # Create your views here.
+def interface (request):
+    return render(request ,'page/index.html')
